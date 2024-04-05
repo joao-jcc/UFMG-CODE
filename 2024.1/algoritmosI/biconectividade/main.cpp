647a59fb7315503b3b883a64acd1aa5d866240e7
João Costa <joaocosta@dcc.ufmg.br>
2024-04-05 15:34:00 -0300

    solved step 1 and 2

--- 2024.1/algoritmosI/biconectividade/main.cpp
+++ 2024.1/algoritmosI/biconectividade/main.cpp
@@ -1,9 +1,24 @@
 #include <vector>
+#include <stack>
 #include <stdio.h>
 
 using namespace std;
 
-void read_graph(vector<int> graph[], int n_vertex) {    
+// estruturas auxiliares para problema da articulação e clusters
+vector<vector<int>> graph; // vetor de vetor para representar o grafo
+
+vector<int> born; // tempos de início zerados
+vector<bool> visited; // true: visitado, false: não-visitado
+vector<int> low; // mínimo tempo de nascimento alcançavel
+
+vector<int> parents;
+vector<vector<int>> components;
+stack<int> s;
+
+int time_ = 0;
+
+
+void read_graph(int n_vertex) {    
     for (int i=0; i < n_vertex; ++i) {
         
         // grau do vértice i
@@ -19,96 +34,118 @@ void read_graph(vector<int> graph[], int n_vertex) {
 }
 
 
-void print_graph(vector<int> graph[], int n_vertex) {
+void DFS_cut(int u, int parent) {
     
-    for (int i = 0; i < n_vertex; ++i) {
-        printf("%d: ", i);
-        for (int j = 0; j < graph[i].size(); ++j) {
-            printf("%d ", graph[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void print_dfs_result(int n_vertex, vector<int>& born_times, vector<int>& end_times, vector<int>& parents, vector<int>& colors) {
-    printf("Vertex\tColor\tParent\tTimes\n");
-    for (int i = 0; i < n_vertex; ++i) {
-        printf("%d\t%d\t%d\t%d / %d\n", i, colors[i], parents[i], born_times[i], end_times[i]);
-    }
-}
-
-
-
-void DFS_visit(vector<int> graph[], int& u, int& time, vector<int>& born_times, 
-vector<int>& end_times, vector<int>& parents, vector<int>& colors) {
-    
-    ++time;
-    colors[u] = 1; // vértice u descoberto colorido de cinza
-    born_times[u] = time; // tempo de descoberta de u
+    visited[u] = true; // vértice u descoberto
+    born[u] = low[u] = ++time_; // tempo de descoberta de u
+    int children = 0; // numero de filhos de u na árvore de precedencia
 
     // percorre-se todos os vértices adjacentes a u
     for (int v : graph[u]) {
+        // vértice adjacente ainda não foi descoberto
+        if (!visited[v]) {
+            // chama-se DFS_cut para o novo vértice descoberto v, cujo pai é u
+            DFS_cut(v, u);
 
-        // vértice adjacente é branco
-        if (colors[v] == 0) {
+            low[u] = min(low[u], low[v]);
 
-            // u é pai de v
-            parents[v] = u;
-            // chama-se DFS_visit para o novo vértice descoberto v
-            DFS_visit(graph, v, time, born_times, end_times, parents, colors);
+            if (parent != -1 && born[u] <= low[v]) {
+                printf("%d ", u); // u é de corte
+            }
 
-        }
+            ++children;
 
-        // vértice adjacente é cinza
-        if (colors[v] == 1) {
-
+        } else {
+            // há aresta de retorno
+            low[u] = min(low[u], born[v]);
         }
     }
-    
-    // todas as arestas de u foram percorridas
-    ++time;
-    end_times[u] = time;
-    colors[u] = 2; // colore-se u de preto
 
+    if (parent == -1 && children >= 2) {
+        printf("%d ", u); // u é de corte    
+    }
 }
 
 
+void DFS_cluster(int u) {
+    born[u] = low[u] = ++time_; // tempo de descoberta de u
+    s.push(u);
 
-void DFS(vector<int> graph[], int n_vertex, int& time, vector<int>& born_times, 
-vector<int>& end_times, vector<int>& parents, vector<int>& colors) {
-    
+    for (int v : graph[u]) {
+        if (born[v] == 0) {
+            parents[v] = u; // u é pai de v
+            
+            DFS_cluster(v);
+
+            low[u] = min(low[u], low[v]);
+
+            if (born[u] <= low[v]) {
+                
+                vector<int> component;
+                int y;
+
+                do {
+                    y = s.top();
+                    s.pop();
+                    component.push_back(y);
+
+                } while (y != v);
+                component.push_back(u);
+                components.push_back(component);
+
+            }
+        } else if (v != parents[u]) {
+            low[u] = min(low[u], born[v]);
+        }
+    }
+}
+
+
+void DFS(int n_vertex) {
     // para cada vértice u
     for (int u=0; u < n_vertex; ++u) {
             // se o vértice adjacente é branco
-            if (colors[u] == 0) {
-                DFS_visit(graph, u, time, born_times, end_times, parents, colors);
+            if ( !visited[u] ) {
+                DFS_cut(u, -1);
             }
         }
-    }
+    printf("\n");
+}
 
 
 int main(void) {
     
     // lendo o número de vértices do grafo
     int n_vertex; scanf(" %d", &n_vertex);
-    // declarando grafo como lista de adjacências: array de vetores
-
-    vector<int> graph[n_vertex];
-    read_graph(graph, n_vertex);
     
+    // Inicializando o vetor graph com tamanho n_vertex
+    graph.resize(n_vertex);
+
+    // declarando grafo como lista de adjacências: array de vetores
+    read_graph(n_vertex);
 
     // arrays auxiliares DFS
-    vector<int> born_times(n_vertex, 0); // tempos de início zerados
-    vector<int> end_times(n_vertex, 0); // tempos de fim zerados
-    vector<int> parents(n_vertex, -1); // -1 indica que o vértice é raiz da árvore de predecessores
-    vector<int> colors(n_vertex, 0); // 0: branco, 1: cinza, 2: preto
-    int time = 0;
+    born.resize(n_vertex, 0); // tempos de início zerados
+    visited.resize(n_vertex, false); // todos os vértices são inicialmente não visitados
+    low.resize(n_vertex, 0); // mínimo tempo de nascimento alcançável
+    parents.resize(n_vertex, -1);
 
-    DFS(graph, n_vertex, time, born_times, end_times, parents, colors);
-
-    // Print the DFS result
-    print_dfs_result(n_vertex, born_times, end_times, parents, colors);
-
+    // DFS(n_vertex);
+    DFS_cluster(0);
+    
+    printf("_______\n");
+    for (const auto& component : components) {
+        for (int v : component) {
+            printf("%d ", v);
+        }
+        
+        printf("\n");
+    }
 
     return 0;
+
 }
+
+
+// gerar variáveis globais
+// colocar macros de competição
